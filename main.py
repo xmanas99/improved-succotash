fe54689d353943e33f5c36b0849f76c6cd421e07
amtempleton <41972672+amtempleton@users.noreply.github.com>
2018-08-16 15:51:19 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ class CssiUser(ndb.Model):
      first_name = ndb.StringProperty()
      last_name = ndb.StringProperty()
 
+class Event(ndb.Model):
+    title_event = ndb.StringProperty()
+    day_event = ndb.IntegerProperty()
+    month_event = ndb.IntegerProperty()
+    year_event = ndb.IntegerProperty()
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         template = jinja_env.get_template('main.html')
@@ -28,7 +34,7 @@ class MainPage(webapp2.RequestHandler):
         else:
             self.response.write(template.render({
                 'logout_url': users.create_logout_url('/'),
-            }))
+             }))
         if not current_user:
             self.redirect(users.create_login_url('/Chronicle'))
 
@@ -37,9 +43,16 @@ class CalendarPage(webapp2.RequestHandler):
         template = jinja_env.get_template('index.html')
         self.response.write(template.render({
             'logout_url': users.create_logout_url('/'),
-            }))
-            
+        }))
+
+
+class AddEvent(webapp2.RequestHandler):
+    def get(self):
+        AddEvent = jinja_env.get_template('add_event.html')
+        self.response.write(AddEvent.render())
+        
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/Chronicle', CalendarPage)
+    ('/Chronicle', CalendarPage),
+    ('/Event', AddEvent)
 ], debug=True)
